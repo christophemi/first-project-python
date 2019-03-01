8f2ea3e5bc65a349b6209ccfb92226cd2c202cae
Christophe <christopheminotti@gmail.com>
2019-03-01 14:38:08 +0100

    test function import

---
+++ function.py
@@ -0,0 +1,6 @@
+def dire(nom_personne, message_personne):
+    print("{} : {}".format(nom_personne, message_personne))
+
+""" pour faire des test si tout marche bien """
+if __name__ == "__main__":
+    dire("gogol", "bonjour")
--- script.py
+++ script.py
@@ -2,8 +2,13 @@
 
 """ commentaire sur
 plusieur ligne """
-
+"""
 print("coucou !")
 print("hola ! \nchico !!") # le \n retour à la ligne
 print("\tcoucou !")  # \t tabulation  
+"""
+from function import dire
+
+dire("julie", "coucou !")
+
 
